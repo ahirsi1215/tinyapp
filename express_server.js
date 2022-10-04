80ff57eb7313ab94bcc001032729b45bbadd05b8
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-10-04 00:13:34 +0000

    added some url functionality

--- express_server.js
+++ express_server.js
@@ -16,7 +16,8 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
