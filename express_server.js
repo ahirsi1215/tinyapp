69d79fcf26c3ba978c99885c657c5d88820c3931
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-10-04 00:44:22 +0000

    added shorl url functionality and header

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -22,6 +23,10 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+  app.get("/urls/:id", (req, res) => {
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    res.render("urls_show", templateVars);
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
