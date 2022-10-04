6fcfbefc9b3325cce7285340f1c06f4a8ac81ed0
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-10-04 20:18:47 +0000

    added a page for creating a new url

--- express_server.js
+++ express_server.js
@@ -4,12 +4,12 @@ const PORT = 8080; // default port 8080
 
 
 app.set("view engine", "ejs");
-
+function generateRandomString() {}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -24,9 +24,16 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
   app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render("urls_show", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
