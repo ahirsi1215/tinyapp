8b45588ec4afd047938dd56e690d730f460984d9
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-10-04 20:40:16 +0000

    implemented random string

--- express_server.js
+++ express_server.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-
 app.set("view engine", "ejs");
-function generateRandomString() {}
+const generateRandomString = function() {
+  let character = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = 6; i > 0; --i) {
+    result += character[Math.floor(Math.random() * character.length)];
+  }
+  return result;
+  }
+  
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -33,7 +41,7 @@ app.get("/urls/new", (req, res) => {
 });
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.send("/urls/:id"); // Respond with 'Ok' (we will replace this)
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
