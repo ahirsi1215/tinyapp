04be84b2456111d22c329b51ae9511fd89b70f06
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-10-07 00:06:01 +0000

    added cookies/logout/login functions will style more later down line

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser())
 app.set("view engine", "ejs");
 // random string function
 const generateRandomString = function () {
@@ -26,14 +29,21 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 //generate random string for url link
@@ -55,6 +65,14 @@ app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;;
   res.redirect("/urls");
 })
+app.post("/login",(req,res) =>{
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+app.post("/logout",(req,res) =>{
+ res.clearCookie("username", req.body.username);
+ res.redirect("urls")
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
