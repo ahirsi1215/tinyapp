4180a264a33ff59257939f8e96dea67ce25770e4
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-10-08 01:49:22 +0000

    refactored head and added users

--- express_server.js
+++ express_server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const { restart } = require("nodemon");
 const cookieParser = require('cookie-parser')
-
+// middleware
 app.use(cookieParser())
+
 app.set("view engine", "ejs");
 // random string function
 const generateRandomString = function () {
@@ -16,6 +17,20 @@ const generateRandomString = function () {
   return result;
 }
 
+//users object
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+// default websites used for edit/test and showing
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -31,21 +46,29 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_new", templateVars);
 });
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id], 
+    user: users[req.cookies["user_id"]]  };
   res.render("urls_show", templateVars);
 });
+// registration page
+app.get("/register", (req, res) => {
+  const templateVars = {user: users[req.cookies["user_id"]] }; 
+  res.render("register", templateVars);
+})
 //generate random string for url link
 app.post("/urls", (req, res) => {
   const shortUrl = generateRandomString();
@@ -54,6 +77,7 @@ app.post("/urls", (req, res) => {
 });
 //redirect to long url
 app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {user: users[req.cookies["user_id"]] };
   res.redirect(urlDatabase[req.params.shortURL]);
 });
 //delete functionality
@@ -65,13 +89,25 @@ app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;;
   res.redirect("/urls");
 })
-app.post("/login",(req,res) =>{
-  res.cookie("username", req.body.username);
+// login
+app.post("/login", (req, res) => {
+  res.cookie("user_id", req.body.userId);
   res.redirect("/urls");
 });
-app.post("/logout",(req,res) =>{
- res.clearCookie("username", req.body.username);
- res.redirect("urls")
+// logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id", req.body.userId);
+  res.redirect("/urls");
+});
+app.post("/register", (req, res) => {
+  const userId = generateRandomString();
+  users[userId] = {
+    id: userId,
+    email: req.body.email,
+    password: req.body.password
+  }
+  res.cookie("user_id", userId)
+  res.redirect("urls");
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
